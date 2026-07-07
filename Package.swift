5502e0f45c363bb6950cc5db28e125b128b39e18
Vamsi Prattipati <vamsipdhar@yahoo.com>
2026-07-07 08:07:17 -0400

    release: 0.9.0

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KernelDefenseVideoProcessor",
-            url: "https://github.com/kerneldefense/videoprocessor/releases/download/0.6.0/KernelDefenseVideoProcessor.xcframework.zip",
-            checksum: "75744a61132334f31fcc29609875fc55d92bf942a5a5e7d4111657ed55fccfbe"
+            url: "https://github.com/kerneldefense/videoprocessor/releases/download/0.9.0/KernelDefenseVideoProcessor.xcframework.zip",
+            checksum: "6ceee5de7eb5e41a02c7a6bc031ca1fd62df624a0795f58eac2c24cef72ff8bf"
         ),
     ]
 )
